8b4b614d698c03c9522b0cdfa40056dc15ae64f2
markhurlburt <markhurlburt@MarkHurlburt.local>
2014-12-07 13:49:15 -0600

    Changed wording in some forms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,25 @@
 //= require jquery-waypoints/waypoints
 //= require litebox/litebox
 
+// jQuery plugin to prevent double submission of forms
+jQuery.fn.preventDoubleSubmission = function() {
+  $(this).on('submit',function(e){
+    var $form = $(this);
+
+    if ($form.data('submitted') === true) {
+      // Previously submitted - don't submit again
+      e.preventDefault();
+    } else {
+      // Mark it so that the next submit can be ignored
+      $form.data('submitted', true);
+    }
+  });
+
+  // Keep chainability
+  return this;
+};
+
+
 $('.section-masthead-logo').waypoint(
     {
       handler: function(direction) {
@@ -99,6 +118,9 @@ if($('body').hasClass("home")&& $('body').hasClass("index")) {
       $('.video-header-video').css("top", -($(window).scrollTop() / 5) + "px");       
   });
 }
+
+// Stop users from inadvertantly spamming forms
+$('form').preventDoubleSubmission();
 
 
 $('.litebox').liteBox({
