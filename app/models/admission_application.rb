62020ae757d0fb0980baaa38d5aa969051ccd8f1
markhurlburt <markhurlburt@MarkHurlburt.local>
2014-12-08 22:54:19 -0600

    added validations for length

--- app/models/admission_application.rb
+++ app/models/admission_application.rb
@@ -19,6 +19,7 @@ class AdmissionApplication < ActiveRecord::Base
 	validates_associated :profile_question_answers, :if => :active?, :message=>"must all be answered."
 	
 	validates :last_name, :first_name, :address, :city, :state, :zip_code, :legal_status, :education, :employment_status, :goal, :referral_source, :phone ,:presence => true, :if => :active_or_general?, length: { maximum: 255 }	
+	validates :website_link, :linkedin_account, :resume_link, :payment_option, :twitter_account, :github_account, :middle_name, length: { maximum: 255 }
 	validates_format_of :website_link, :linkedin_account, :with => URI::regexp(%w(http https)), allow_blank: true, message: "is not a valid URL", :if => :active_or_general?
 	validates_format_of :resume_link, :with => URI::regexp(%w(http https)), allow_blank: true, message: "is not a valid URL", :if => :active?
 	validates_format_of :phone, with: /\d{3}-\d{3}-\d{4}/, message: "is not formatted properly", allow_blank: true
