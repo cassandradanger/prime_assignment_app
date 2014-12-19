abc37d065c341a0afa3a79d088351cbf1e04e286
markhurlburt <markhurlburt@dhcp-08-58.bravo>
2014-12-19 10:03:00 -0600

    forced ranges to use normalized dates

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,14 @@
-class DashboardController < AdminApplicationController
+class DashboardController < ApplicationController
   require('groupdate')
-
+  before_filter :authenticate_admin!
 
   def index
-    @user_count = User.count
-    @apps_count = AdmissionApplication.started.count
+
+    @apps_count = AdmissionApplication.count
     @apps_completed_count = AdmissionApplication.completed.count
-    @apps_by_create_date60 = AdmissionApplication.started.where('created_at > ?',60.days.ago).group_by_day(:created_at).count
-    @apps_by_completed_date60 = AdmissionApplication.completed.where('created_at > ?',60.days.ago).group_by_day(:created_at).count
-    @apps_by_referral_source = AdmissionApplication.has_referral.group(:referral_source).count
-    @apps_by_referral_source_date60 = AdmissionApplication.has_referral.where('created_at > ?',60.days.ago).group(:referral_source).group_by_day(:created_at).count
+    @apps_by_create_date60 = AdmissionApplication.where('created_at > ?',60.days.ago).group_by_day(:created_at, range: 60.days.ago.midnight..Date.today.midnight, format: "%b %-e").count
+    @apps_by_completed_date60 = AdmissionApplication.completed.where('created_at > ?',60.days.ago).group_by_day(:created_at, range: 60.days.ago.midnight..Date.today.midnight, format: "%b %-e").count
+    @apps_by_referral_source = AdmissionApplication.group(:referral_source).count
+    @apps_by_referral_source_date60 = AdmissionApplication.where('created_at > ?',60.days.ago).group(:referral_source).group_by_day(:created_at, range: 60.days.ago.midnight..Date.today.midnight, format: "%b %-e").count
   end
 end
