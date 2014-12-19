e070330e9ea3e0bf88aa3e1d3e666249e70cce00
markhurlburt <markhurlburt@MarkHurlburt.local>
2014-12-19 11:16:13 -0600

    Added zipnosis

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,9 +6,9 @@ class DashboardController < ApplicationController
 
     @apps_count = AdmissionApplication.count
     @apps_completed_count = AdmissionApplication.completed.count
-    @apps_by_create_date60 = AdmissionApplication.where('created_at > ?',60.days.ago).group_by_day(:created_at, range: 60.days.ago.midnight..Date.today.midnight, format: "%b %-e").count
-    @apps_by_completed_date60 = AdmissionApplication.completed.where('created_at > ?',60.days.ago).group_by_day(:created_at, range: 60.days.ago.midnight..Date.today.midnight, format: "%b %-e").count
+    @apps_by_create_date60 = AdmissionApplication.where('created_at > ?',60.days.ago).group("DATE(created_at)").count
+    @apps_by_completed_date60 = AdmissionApplication.completed.where('created_at > ?',60.days.ago).group_by_day(:created_at).count
     @apps_by_referral_source = AdmissionApplication.group(:referral_source).count
-    @apps_by_referral_source_date60 = AdmissionApplication.where('created_at > ?',60.days.ago).group(:referral_source).group_by_day(:created_at, range: 60.days.ago.midnight..Date.today.midnight, format: "%b %-e").count
+    @apps_by_referral_source_date60 = AdmissionApplication.where('created_at > ?',60.days.ago).group(:referral_source).group_by_day(:created_at).count
   end
 end
