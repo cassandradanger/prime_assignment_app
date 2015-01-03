fcf6701dee1367efc01ea4a29aad4404e455312c
Mike Schmidt <mikes@nerdery.com>
2015-01-03 11:19:35 -0600

    Add filtering to admission application screen.

--- app/controllers/admission_applications_controller.rb
+++ app/controllers/admission_applications_controller.rb
@@ -4,15 +4,13 @@ class AdmissionApplicationsController < AdminApplicationController
   respond_to :html
 
   def index
-    @cohort_id = params[:cohort_id] || nil
-    if @cohort_id
-      @admission_applications = Cohort.find(params[:cohort_id]).admission_applications.all
-    else
-      @admission_applications = AdmissionApplication.all
-    end
-    @cohort_id = params[:cohort_id]
+    set_default_params(:cohort)
+    set_default_params(:app_status)
+
+    @admission_applications = AdmissionApplication.filter(params.slice(:cohort,:app_status))
+    @cohort_id = params[:cohort]
+    @app_status = params[:app_status]
     @cohorts = Cohort.all
-    respond_with(@admission_applications)
   end
 
   def show
@@ -44,6 +42,12 @@ class AdmissionApplicationsController < AdminApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def admission_application_params
       params.require(:admission_application).permit(:first_name, :middle_name, :last_name, :phone, :address, :city, :state, :zip_code, :legal_status, :education, :employment_status, :goal, :income, :linkedin_account, :twitter_account, :github_account, :website_link, :referral_source, :resume_link, :payment_plan, :payment_option, :logic_question_answers_attributes => [:id,:logic_question_id,:answer,:explanation], :profile_question_answers_attributes => [:id,:profil_question_id,:answer,:score], :cohort_ids => [])
+    end
+
+    def set_default_params(param)
+      session_name = "admission_application_#{param}_filter".to_sym
+      params[param] ||= session[session_name]
+      session[session_name] = params[param]
     end
 
 end
