13dde7a9c50c3cd642e6a6476f5d6ccd923ac484
markhurlburt <markhurlburt@dhcp-08-215.bravo>
2015-01-05 11:51:29 -0600

    whitelisted resume notes

--- app/controllers/admission_applications_controller.rb
+++ app/controllers/admission_applications_controller.rb
@@ -41,7 +41,7 @@ class AdmissionApplicationsController < AdminApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def admission_application_params
-      params.require(:admission_application).permit(:first_name, :middle_name, :last_name, :phone, :address, :city, :state, :zip_code, :legal_status, :education, :employment_status, :goal, :income, :linkedin_account, :twitter_account, :github_account, :website_link, :referral_source, :resume_link, :payment_plan, :payment_option, :resume_score, :logic_question_answers_attributes => [:id,:logic_question_id,:answer,:explanation], :profile_question_answers_attributes => [:id,:profil_question_id,:answer,:score], :cohort_ids => [])
+      params.require(:admission_application).permit(:first_name, :middle_name, :last_name, :phone, :address, :city, :state, :zip_code, :legal_status, :education, :employment_status, :goal, :income, :linkedin_account, :twitter_account, :github_account, :website_link, :referral_source, :resume_link, :payment_plan, :payment_option, :resume_score, :resume_notes, :logic_question_answers_attributes => [:id,:logic_question_id,:answer,:explanation], :profile_question_answers_attributes => [:id,:profil_question_id,:answer,:score], :cohort_ids => [])
     end
 
     def set_default_params(param)
