cc7a90c9560c4d7f8268c89300fab36f9173d11d
Mike Schmidt <mikes@nerdery.com>
2015-01-13 16:35:43 -0600

    Adjust date formats for line charts on dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,11 +7,11 @@ class DashboardController < AdminApplicationController
     @apps_count = AdmissionApplication.started.count
     @apps_completed_count = AdmissionApplication.completed.count
     @apps_accepted_count = AdmissionApplication.accepted.count
-    @apps_by_create_date60 = AdmissionApplication.started.where('created_at > ?',60.days.ago).group_by_day(:created_at, format: "%m/%d").count
+    @apps_by_create_date60 = AdmissionApplication.started.where('created_at > ?',60.days.ago).group_by_day(:created_at, format: "%m/%d/%Y").count
     @apps_by_completed_date60 = AdmissionApplication.completed.where('created_at > ?',60.days.ago).group_by_day(:created_at, format: "%m/%d").count
     @apps_by_accepted_date60 = AdmissionApplication.accepted.where('created_at > ?',60.days.ago).group_by_day(:created_at, format: "%m/%d").count
     @apps_by_payment_option = AdmissionApplication.completed.group(:payment_option).count
     @apps_by_referral_source = AdmissionApplication.has_referral.group(:referral_source).count
-    @apps_by_referral_source_date7 = AdmissionApplication.has_referral.where('created_at >= ?',7.days.ago).group(:referral_source).group_by_day(:created_at, format: "%m/%d").count
+    @apps_by_referral_source_date7 = AdmissionApplication.has_referral.where('created_at >= ?',14.days.ago).group(:referral_source).group_by_day(:created_at, format: "%m/%d/%Y").count
   end
 end
