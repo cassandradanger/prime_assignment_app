aa2177a2d188368f82533860b35510ff71279fe5
Mike Schmidt <mikes@nerdery.com>
2015-01-13 16:26:35 -0600

    Add additional status codes to the applications.

--- app/controllers/admission_applications_controller.rb
+++ app/controllers/admission_applications_controller.rb
@@ -8,6 +8,7 @@ class AdmissionApplicationsController < AdminApplicationController
     set_default_params(:app_status)
 
     @admission_applications = AdmissionApplication.filter(params.slice(:cohort,:app_status))
+    @status_options = AdmissionApplication.application_status_options
     @cohort_id = params[:cohort]
     @app_status = params[:app_status]
     @cohorts = Cohort.all
@@ -21,7 +22,7 @@ class AdmissionApplicationsController < AdminApplicationController
   end
 
   def update
-    @admission_application.update(admission_application_params)
+    flash[:success] = "Application updated successfully!" if @admission_application.update(admission_application_params)
     respond_with(@admission_application)
   end
 
@@ -41,7 +42,7 @@ class AdmissionApplicationsController < AdminApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def admission_application_params
-      params.require(:admission_application).permit(:first_name, :middle_name, :last_name, :phone, :address, :city, :state, :zip_code, :legal_status, :education, :employment_status, :goal, :income, :linkedin_account, :twitter_account, :github_account, :website_link, :referral_source, :resume_link, :payment_plan, :payment_option, :resume_score, :resume_notes, :logic_question_answers_attributes => [:id,:logic_question_id,:answer,:explanation], :profile_question_answers_attributes => [:id,:profil_question_id,:answer,:score], :cohort_ids => [])
+      params.require(:admission_application).permit(:first_name, :middle_name, :last_name, :phone, :address, :city, :state, :zip_code, :legal_status, :education, :employment_status, :goal, :income, :linkedin_account, :twitter_account, :github_account, :website_link, :referral_source, :resume_link, :payment_plan, :payment_option, :resume_score, :resume_notes, :application_status, :logic_question_answers_attributes => [:id,:logic_question_id,:answer,:explanation], :profile_question_answers_attributes => [:id,:profil_question_id,:answer,:score], :cohort_ids => [])
     end
 
     def set_default_params(param)
