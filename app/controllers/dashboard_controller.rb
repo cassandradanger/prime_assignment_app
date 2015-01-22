00b71dd59d7566984133fdccd6106dbf7f1d89e0
markhurlburt <markhurlburt@MarkHurlburt.local>
2015-01-22 11:11:46 -0600

    updated graph data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,7 +8,7 @@ class DashboardController < AdminApplicationController
     @apps_completed_count = AdmissionApplication.completed.count
     @apps_accepted_count = AdmissionApplication.accepted.count
     @apps_by_create_date60 = AdmissionApplication.started.where('created_at > ?',60.days.ago).group_by_day(:created_at, format: "%m/%d/%Y").count
-    @apps_by_completed_date60 = AdmissionApplication.completed.where('created_at > ?',60.days.ago).group_by_day(:completed_at, format: "%m/%d").count
+    @apps_by_completed_date60 = AdmissionApplication.completed.where('completed_at > ?',60.days.ago).group_by_day(:completed_at, format: "%m/%d/%Y").count
     @apps_by_accepted_date60 = AdmissionApplication.accepted.where('created_at > ?',60.days.ago).group_by_day(:created_at, format: "%m/%d").count
     @apps_by_payment_option = AdmissionApplication.completed.group(:payment_option).count
     @apps_by_referral_source = AdmissionApplication.has_referral.group(:referral_source).count
