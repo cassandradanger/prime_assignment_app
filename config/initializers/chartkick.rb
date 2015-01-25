2e0b42d7b39053487b355c7271a590eabd9d6326
Mike Schmidt <mikes@nerdery.com>
2015-01-25 16:32:19 -0600

    Add education pie chart to the dashboard.  Change the color of the charts on the dashboard

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,3 +1,17 @@
 # Initializer for Chartkick
 
-Chartkick.options[:content_for] = :charts_js
+# Chartkick.options[:content_for] = :charts_js
+
+Chartkick.options = {
+    content_for: :charts_js,
+    colors: [
+        '#d3d3d3',
+        '#bababa',
+        '#79d2c0',
+        '#07AA9E',
+        '#168DAA',
+        '#0A9186',
+        '#128CBC',
+        '#0DD8D5',
+        '#055160']
+}
