9ff102e2ba3873dea22350206a3733576020d6fa
Mike Schmidt <mikes@nerdery.com>
2015-01-26 17:20:52 -0600

    Origin Header gem allow all

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,9 +99,5 @@ Rails.application.configure do
     }  
   }
 
-  config.action_dispatch.default_headers.merge!({
-      'Access-Control-Allow-Origin' =>  ENV['ASSET_HOST'],
-      'Access-Control-Request-Method' => '*'
-  })
 
 end
