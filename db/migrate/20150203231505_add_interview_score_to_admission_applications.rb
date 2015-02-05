95c4cfbc38931c95cb97381afde979cd2556f56f
Mike Schmidt <mikes@nerdery.com>
2015-02-05 16:03:02 -0600

    Rework the migration so the update happens last.

--- db/migrate/20150203231505_add_interview_score_to_admission_applications.rb
+++ db/migrate/20150203231505_add_interview_score_to_admission_applications.rb
@@ -3,11 +3,5 @@ class AddInterviewScoreToAdmissionApplications < ActiveRecord::Migration
     add_column :admission_applications, :interview_score, :integer, default: 0
   end
 
-  def data
-    apps = AdmissionApplication.where(application_status: "interview_passed")
-    apps.each do |app|
-      app.interview_score = 3
-      app.save
-    end
-  end
+
 end
--- db/migrate/20150205194010_add_assigned_cohort_id_to_admission_applications.rb
+++ db/migrate/20150205194010_add_assigned_cohort_id_to_admission_applications.rb
@@ -2,4 +2,12 @@ class AddAssignedCohortIdToAdmissionApplications < ActiveRecord::Migration
   def change
     add_column :admission_applications, :assigned_cohort_id, :integer, index: true
   end
+
+  def data
+    apps = AdmissionApplication.where(application_status: "interview_passed")
+    apps.each do |app|
+      app.interview_score = 3
+      app.save
+    end
+  end
 end
