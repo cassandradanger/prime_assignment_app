54656ea9db158c9f9173eaebc6d08c6820cbcdd8
markhurlburt <markhurlburt@MarkHurlburt.local>
2015-02-08 19:32:26 -0600

    Added inverse notes on cohort dashboard.

--- app/models/admission_application.rb
+++ app/models/admission_application.rb
@@ -11,6 +11,7 @@ class AdmissionApplication < ActiveRecord::Base
   scope :cohort, lambda { |n| joins(:cohorts).where('cohorts.id = ?', n) }
 
   # scope :aid_eligible, -> { where(income: 0...23340) }
+  scope :aid_ineligible, -> { where("(income > 23341 AND (dependents = 0 OR dependents IS NULL)) OR (income > 31460 AND dependents = 1) OR (income > 39580 AND dependents = 2) OR (income > 47700 AND dependents = 3) OR (income > 55820 AND dependents = 4) OR (income > 63940 AND dependents = 5) OR (income > 72060 AND dependents = 6) OR (income > 80180 AND dependents = 7)") }  
   scope :aid_eligible, -> { where("(income <= 23341 AND (dependents = 0 OR dependents IS NULL)) OR (income <= 31460 AND dependents = 1) OR (income <= 39580 AND dependents = 2) OR (income <= 47700 AND dependents = 3) OR (income <= 55820 AND dependents = 4) OR (income <= 63940 AND dependents = 5) OR (income <= 72060 AND dependents = 6) OR (income <= 80180 AND dependents = 7)") }
   scope :aid_seeking, -> { where(payment_option: "Scholarship") }
   scope :not_aid_seeking, -> { where.not(payment_option: "Scholarship") }
