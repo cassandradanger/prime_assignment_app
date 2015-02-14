fad19a8ee468dc1f1758684a96116f8201309fab
markhurlburt <markhurlburt@MarkHurlburt.local>
2015-02-14 10:21:13 -0600

    Added additional documents

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,9 @@ Rails.application.routes.draw do
   get '/live', to: 'home#live'
   get '/jobs', to: 'home#jobs'
   get '/catalog', to: redirect('/assets/documents/catalog.pdf')
+  get '/conduct', to: redirect('/assets/documents/conduct_guide.pdf')
+  get '/program', to: redirect('/assets/documents/program_description.pdf')
+
 
   root 'home#index'
 
